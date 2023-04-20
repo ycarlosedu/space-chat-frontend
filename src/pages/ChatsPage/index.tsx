92f589efba974c4c41e0f676c641c5ae352eb5c4
Carlos Eduardo <silvacarlos02@sicredi.com.br>
2023-04-20 10:05:11 -0300

    feat: added tailwindcss

--- src/pages/ChatsPage/index.tsx
+++ src/pages/ChatsPage/index.tsx
@@ -4,7 +4,7 @@ const ChatsPage = (props: any) => {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <PrettyChatWindow
-        projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID || ""}
+        projectId={import.meta.env.REACT_APP_CHAT_ENGINE_PROJECT_ID || ""}
         username={props.user.username}
         secret={props.user.secret}
         style={{ height: "100%" }}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  important: true,
+  content: [
+    './src/**/*.tsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
