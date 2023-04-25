96d3a874e7b6dec8cccd235a9356258d9042b844
Carlos Eduardo <silvacarlos02@sicredi.com.br>
2023-04-25 18:40:40 -0300

    fix: env variables netlify

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {viteCommonjs, esbuildCommonjs} from '@originjs/vite-plugin-commonjs'
 
+const viteEnv = {} 
+Object.keys(process.env).forEach((key) => { 
+  if (key.startsWith('VITE_')) { 
+    viteEnv[`import.meta.env.${key}`] = process.env[key] 
+  } 
+})
+
 export default defineConfig({
   plugins: [viteCommonjs(), react()],
   build: {
