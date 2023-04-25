8130d1fa4cc4f4a55bd8c59a5ee38220517e9b00
Carlos Eduardo <silvacarlos02@sicredi.com.br>
2023-04-25 15:45:20 -0300

    fix: require in prod

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// @ts-ignore
+window.require = window.import;
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   // <React.StrictMode>
     <App />
